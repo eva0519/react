e9fff2304fb4051d4e590a7b43cc73972d5c578b
eva0519 <kimyuntae19@gmail.com>
2019-12-04 15:39:46 +0900

    styled-component expression syntex hilighting

---
+++ styling-react/src/StyledComponent.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+//스타일드컴포넌트 라이브러리 css in js 방식. 백틱사용.
+
+const Box = styled.div`
+  background: ${props => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
+`;
+const Button = styled.button`
+  background: white;
+  color: black;
+  border-radius: 4px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  font-size: 1rem;
+  font-weight: 600;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.9);
+  }
+
+  ${props =>
+    props.inverted &&
+    css`
+      background: none;
+      border: 2px solid white;
+      color: white;
+      &:hover {
+        background: white;
+        color: black;
+      }
+    `};
+  & + button {
+    margin-left: 1rem;
+  }
+`;
+
+const StyledComponent = () => (
+  <Box color="black">
+    <Button>안녕하세요</Button>
+    <Button inverted={true}>테두리만</Button>
+  </Box>
+);
+
+export default StyledComponent;
