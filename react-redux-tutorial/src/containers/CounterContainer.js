d886c7ff15cdc744ad924c79005b3c9d39e53ed6
eva0519 <kimyuntae19@gmail.com>
2019-12-14 01:52:16 +0900

    react-redux connect

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Counter from '../components/Counter';
-import {connect} from 'react-redux';
-import {increase, decrease} from '../modules/counter';
+import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = ({ number, increase, decrease }) => {
   return (
-  <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
 };
 
 export default connect(
   state => ({
     number: state.counter.number,
-  }),  // mapStateToProps
-  dispatch => ({
-    increase: () => dispatch(increase()),
-    decrease: () => dispatch(decrease()),
-  }), // mapDispatchToProps
+  }), // mapStateToProps
+  {
+    increase,
+    decrease,
+  },
 )(CounterContainer);
